f8210ad9bc70ce1d95b96b93d7ddcaac2cbbdb55
DrakurRedragon <108012915+DrakurRedragon@users.noreply.github.com>
2023-10-03 16:07:09 -0400

    Update ComplianceStandardsRMON.ps1

--- ComplianceStandardsRMON.ps1
+++ ComplianceStandardsRMON.ps1
@@ -1,7 +1,6 @@
 Read-Host "This will assist in the generation of results for Standards Audit."
 Read-Host "207.1.2 Ensure Modern Authentication for Exchange Online is Enabled"
 $org = Read-Host "Please enter the organization: "
-$cred = Get-Credential
 Connect-ExchangeOnline -Organization $org -Credential $cred
 get-organizationconfig | format-table -auto name, OAuth*
 Read-Host "216.2.5 Ensure Office 365 SharePoint Infected files are disallowed for download."
