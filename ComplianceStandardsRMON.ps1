d480e4fcea311200409fa2c0a11c1d62eedbbb35
DrakurRedragon <108012915+DrakurRedragon@users.noreply.github.com>
2023-10-03 16:58:52 -0400

    Update ComplianceStandardsRMON.ps1
    239.4.9

--- ComplianceStandardsRMON.ps1
+++ ComplianceStandardsRMON.ps1
@@ -1,8 +1,9 @@
 Read-Host "This will assist in the generation of results for Standards Audit."
-Read-Host "207.1.2 Ensure Modern Authentication for Exchange Online is Enabled"
+Read-Host "207.1.2 Ensure Modern Authentication for Exchange Online is Enabled and 239.4.9 Ensure Bastic Authentication for Exchange online is disabled"
 $org = Read-Host "Please enter the organization: "
 Connect-ExchangeOnline -Organization $org
 get-organizationconfig | format-table -auto name, OAuth*
+Get-User -ResultSize Unlimited | Select-Object UserPrincipalName, AuthenticationPolicy
 Read-Host "216.2.5 Ensure Office 365 SharePoint Infected files are disallowed for download."
 $adminsite = Read-Host "Please enter the prefix for the Sharepoint admin site: "
 Connect-SPOService -url https://$adminsite-admin.sharepoint.com
