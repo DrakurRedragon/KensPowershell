d8ce457c89ad6bf87dae8a8a52e9b5ec2f51898d
DrakurRedragon <108012915+DrakurRedragon@users.noreply.github.com>
2023-10-03 16:52:45 -0400

    Update ComplianceStandardsRMON.ps1
    234.4.4

--- ComplianceStandardsRMON.ps1
+++ ComplianceStandardsRMON.ps1
@@ -1,7 +1,7 @@
 Read-Host "This will assist in the generation of results for Standards Audit."
 Read-Host "207.1.2 Ensure Modern Authentication for Exchange Online is Enabled"
 $org = Read-Host "Please enter the organization: "
-Connect-ExchangeOnline -Organization $org -Credential $cred
+Connect-ExchangeOnline -Organization $org
 get-organizationconfig | format-table -auto name, OAuth*
 Read-Host "216.2.5 Ensure Office 365 SharePoint Infected files are disallowed for download."
 $adminsite = Read-Host "Please enter the prefix for the Sharepoint admin site: "
@@ -10,3 +10,6 @@ Get-SPOTenant | Select-Object DisallowInfectedFileDownload
 Read-Host "230.3.8 Ensure that Microsoft Teams is using OneDrive for Business and SharePoint for meeting recordings"
 Connect-MicrosoftTeams
 Get-CsTeamsMeetingPolicy -identity global | fl RecordingStorageMode
+Read-Host "234.4.4 Ensure Automatic Forwarding Options are disabled"
+Connect-ExchangeOnline -Organization $org
+Get-RemoteDomain Default | fl AllowedOOFType, AutoForwardEnabled
