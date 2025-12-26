61f31c387df15a1976ed0c610d49f999ae36d80a
Ken Nihart <ken.nihart@gmail.com>
2025-12-26 13:37:22 -0500

    .

--- ChangeMainDomainfor365.ps1
+++ ChangeMainDomainfor365.ps1
@@ -10,8 +10,11 @@ $newdc = Read-Host "Enter new Domain DC IP"
 
 $azureadid = get-azureaduser -SearchString $uname | select objectID
 $azureadupn = get-azureaduser -ObjectId $azureadid.ObjectId | select UserPrincipalName
-$user = Get-ADUser -Identity $uname
+$user = Get-ADUser -Identity eroman
 $guid = $user.ObjectGUID
 $immutableId = [System.Convert]::ToBase64String($guid.ToByteArray())
 
 set-azureaduser -ObjectId $azureadid.ObjectId -ImmutableID $immutableID
+
+graph scope user.readwrite.all
+Update-MgUser -UserId "user@domain.com" -OnPremisesImmutableId $null
