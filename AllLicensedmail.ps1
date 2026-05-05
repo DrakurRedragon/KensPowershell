9f3f4dff02046d914c4f79f30999bbeffbd933e2
DrakurRedragon <108012915+DrakurRedragon@users.noreply.github.com>
2026-05-05 09:11:55 -0400

    Create new dynamic distribution group for orthoct

--- AllLicensedmail.ps1
+++ AllLicensedmail.ps1
@@ -1,1 +1,2 @@
-New-DynamicDistributionGroup -Name "premier.hoppercomms" -Alias "premier.hoppercomms" -RecipientFilter {(RecipientTypeDetails -eq 'UserMailbox' -and AccountDisabled -eq $false)} -HiddenFromAddressListsEnabled $true
+New-DynamicDistributionGroup -Name "orthoct.hoppercomms" -Alias "orthoct.hoppercomms" -PrimarySMTPAddress "orthoct.hoppercomms@myorthoct.com" -RecipientFilter {(RecipientTypeDetails -eq 'UserMailbox' -and AccountDisabled -eq $false)}
+Set-DynamicDistributionGroup -identity "orthoct.hoppercomms" -HiddenFromAddressListsEnabled $true -RequireSenderAuthenticationEnabled $false
